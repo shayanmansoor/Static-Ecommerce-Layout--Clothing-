ee35d6638e80ba0ac79367b676ec02b42b3e3a4c
shayanmansoor <shayanma634@gmail.com>
2026-01-14 11:30:40 +0500

    update commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function feedbackMsg(){
+    
+    let username = document.getElementById("username").value
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone").value
+    let msg = document.getElementById("msg").value
+ 
+    console.log(username);
+    console.log(email);
+    console.log(phone);
+    console.log(msg);
+
+    alert("Your Feedback Submitted successfully")
+
+}
+
